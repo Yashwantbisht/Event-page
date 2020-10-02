32bf150855ee48cb54fd7cbe2d75b10ae026fa19
Yashwantbisht <72188791+Yashwantbisht@users.noreply.github.com>
2020-10-02 07:38:12 +0530

    A programming code

---
+++ Bisht.py
@@ -0,0 +1,8 @@
+# https://www.facebook.com/yashwant/posts/5431525975
+# subscribed by yashwant
+In [48]: a = ''
+In [49]: a.upper?
+Type:       builtin_function_or_method
+String Form:<built­in method upper of str object at 0x7f1c426e0508>
+Docstring:
+S.upper() ­> string
