1ad3df1e172e92ecb2d0a772a4928d99a4b5e18e
Yashwantbisht <72188791+Yashwantbisht@users.noreply.github.com>
2020-10-02 07:43:04 +0530

    A Python Code

---
+++ Yashwant.py
@@ -0,0 +1,10 @@
+# https://www.facebook.com/yashwant/posts/2468521573
+# subscribed by Yashwant
+from numpy import linspace, sin, exp, pi
+import matplotlib.pyplot as mp
+# calculate 500 values for x and y without a for loop
+x = linspace(0, 10*pi, 500)
+y = sin(x) * exp(-x/10)
+# make diagram
+mp.plot(x,y)
+mp.show()
